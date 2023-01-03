8648da7417e1826d03854cb6ef35fa1c57f4fee0
Paulo Gesualdo <paulo.gesualdo@outlook.com>
2023-01-03 18:54:27 -0300

    feat: added new random bucket object

--- main.tf
+++ main.tf
@@ -37,4 +37,15 @@ resource "aws_s3_bucket_object" "my-test-bucket-object" {
   key    = "config/${local.ip_filepath}"
   source = local.ip_filepath
   etag = filemd5(local.ip_filepath)
+  tags = local.common_tags
+  content_type = "application/json"
+}
+
+resource "aws_s3_bucket_object" "random" {
+  bucket = aws_s3_bucket.my-test-bucket
+  key    = "config/${random_pet.bucket.id}.json"
+  source = local.ip_filepath
+  etag = filemd5(local.ip_filepath)
+  tags = local.common_tags
+  content_type = "application/json"
 }
